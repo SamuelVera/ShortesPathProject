985ef5e141eef65417b1fd461e68eee106c63be5
SamuelVera <samuelvera1999@gmail.com>
2019-11-06 00:13:59 -0400

    FXML HomeScreen added with dummy buttons

--- src/main/main.java
+++ src/main/main.java
@@ -11,15 +11,11 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author samue
- */
 public class main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("HomeScreenDocument.fxml"));
         
         Scene scene = new Scene(root);
         
